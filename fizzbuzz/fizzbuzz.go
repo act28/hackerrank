651d5b9c168496ff873bf5f1f6b450a8d5679762
Ignatius Teo <iggy@codeworx.com.au>
2022-04-02 16:19:54 +1000

    Fizzbuzz solution

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	FizzBuzz(18)
+}
+
+func FizzBuzz(n int32) {
+	var i int32
+	for i = 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+
+			fmt.Println(i)
+		}
+	}
+}
