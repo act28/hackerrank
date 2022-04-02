2acfe92a89bcfe2782bfeb4cf923414e7cf8b958
Ignatius Teo <iggy@codeworx.com.au>
2022-04-02 16:20:18 +1000

    DiagonalDifference solution

---
+++ diagonaldifference/diagonaldifference_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDiagonalDifference(t *testing.T) {
+	testCases := []struct {
+		in  [][]int32
+		exp int32
+	}{
+		{
+			in:  [][]int32{{11, 2, 4}, {4, 5, 6}, {10, 8, -12}},
+			exp: 15,
+		},
+		{
+			in:  [][]int32{{11, 2, 4, 6}, {4, 5, 6, -2}, {10, 8, -12, 7}, {-3, 5, 7, 9}},
+			exp: 4,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			output := diagonalDifference(tc.in)
+			assert.Equal(t, tc.exp, output)
+		})
+	}
+}
