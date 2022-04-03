7f3370ec9d78f588250d8c9323c6256518e38dd3
Ignatius Teo <iggy@codeworx.com.au>
2022-04-03 13:26:07 +1000

    BirthdayCakeCandles solution

---
+++ birthdaycandles/bitrthdaycandles_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStaircase(t *testing.T) {
+	testCases := []struct {
+		in  []int32
+		exp int32
+	}{
+		{
+			in:  []int32{4, 4, 2, 1},
+			exp: 2,
+		},
+		{
+			in:  []int32{3, 2, 1, 3},
+			exp: 2,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("birthday cake candles", func(t *testing.T) {
+			assert.Equal(t, tc.exp, birthdayCakeCandles(tc.in))
+		})
+	}
+}
